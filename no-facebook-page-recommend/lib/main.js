7566d1916b8ceb551bd451616e68f39700b9aa2c
Qing-Cheng Li <qc.linux@gmail.com>
2013-03-02 00:34:01 +0800

    firefox

---
+++ no-facebook-page-recommend/lib/main.js
@@ -0,0 +1,12 @@
+var pageMod = require("sdk/page-mod");
+var data = require("sdk/self").data;
+
+pageMod.PageMod({
+  include: "*.facebook.com",
+  contentScript:
+    'var s = document.createElement("script");'+
+    's.src = "'+data.url("kerker.js")+'";'+
+    '(document.head||document.documentElement).appendChild(s);',
+  contentStyleFile: data.url("remove.css")
+});
+
