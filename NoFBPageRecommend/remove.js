28bb77ad2b0882d78228b97aa2ee6bcf411e0edd
Qing-Cheng Li <qc.linux@gmail.com>
2013-03-07 15:01:51 +0800

    add some description and remove more ads

--- NoFBPageRecommend/remove.js
+++ NoFBPageRecommend/remove.js
@@ -1,5 +1,26 @@
+/* 
+    Remove Facebook Suggested Pages & Posts 
 
-var s = document.createElement("script");
-s.src = chrome.extension.getURL("kerker.js");
-(document.head||document.documentElement).appendChild(s);
+    Qing-Cheng Li
 
+*/
+
+
+/* 
+    In order to override the HTMLUlistElement.appendChild function for 
+    removing <li> of suggested pages/posts, so inject the script kerker.js
+    into page. After overriding the function, everytime when a ajax call
+    done, facebook add new <li> into news feed will call appendChild,
+    at that time, my script will check if the <li> is a suggested post or 
+    not.
+*/
+var script = document.createElement("script");
+script.src = chrome.extension.getURL("kerker.js");
+(document.head||document.documentElement).appendChild(script);
+
+/*
+var mayHasAdsDiv = document.getElementsByClassName("ego_column");
+for(var i=0;i<mayHasAdsDiv.length;i++){
+    var div = mayHasAdsDiv[i];
+    var mayContainAdsDiv = div.getElementsByClassName("ego")
+}*/
