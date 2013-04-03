520c1e74c6f0c7aaa0ec1900506252de98103feb
Qing-Cheng Li <qc.linux@gmail.com>
2013-04-03 09:52:40 +0800

    prepare new feature

--- NoFBPageRecommend/background.js
+++ NoFBPageRecommend/background.js
@@ -8,6 +8,25 @@ function checkForValidUrl(tabId, changeInfo, tab) {
 
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
 
+function parser(data){
+    console.log(data.length);
+}
+
+$(function(){
+    console.log("Hello World!");
+
+    $.ajax({
+        type:"get",
+        dataType:"jsonp",
+        url:"http://spamgroup.tonyq.org/groups/jsonp/?jsonp=parser",
+        error:function(){
+            console.log("error");
+        }
+    });
+    
+});
+
+/*
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-3607701-4']);
 _gaq.push(['_trackPageview']);
@@ -17,4 +36,4 @@ _gaq.push(['_trackPageview']);
     ga.src = 'https://ssl.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
-
+*/
