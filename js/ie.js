a7b733a03341792146b9dfdfd95774ed09c651d6
Qing-Cheng Li <qc.linux@gmail.com>
2013-04-07 20:15:08 +0800

    ie test url

--- js/ie.js
+++ js/ie.js
@@ -3,6 +3,8 @@
 
 var qclean_version = "0.2.5.0";
 
+alert(document.URL);
+
 var qclean_fb_url_regexp = new RegExp("^(http://|https://).*\.facebook\.com/");
 
 if(qclean_fb_url_regexp.test(document.URL)){
