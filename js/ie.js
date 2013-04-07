ec0e66e194c5571c0de1c9f50bd0c3305dae95e9
Qing-Cheng Li <qc.linux@gmail.com>
2013-04-07 20:10:45 +0800

    ie test url

--- js/ie.js
+++ js/ie.js
@@ -1,5 +1,12 @@
 /*
 */
 
-alert("QCLean!")
-console.log("Load QCLean IE version.");
+var qclean_version = "0.2.5.0";
+
+var qclean_fb_url_regexp = new RegExp("^(http://|https://).*\.facebook\.com/");
+
+if(qclean_fb_url_regexp.test(document.URL)){
+    console.log("Load QCLean IE version.");
+
+}
+
