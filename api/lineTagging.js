1c5919ab751034ff068e09ab43ecc8c464732eae
Qing-Cheng Li <qc.linux@gmail.com>
2014-03-11 23:06:50 +0800

    build api

---
+++ api/lineTagging.js
@@ -0,0 +1,3 @@
+console.log("Load qclean lineTaggin API");
+var qclean = qclean || {};
+qclean.lineRegExp = new RegExp("(加ID：|加賴|請加LINE|請加我的LINE|麻煩加我LINE|加一下LINE|訂購加LINE|請加 LINE|請加我LINE)+","i");
---
+++ api/story.js
@@ -0,0 +1,9 @@
+console.log("Load qclean story API");
+var qclean = qclean || {};
+qclean.storyClassNames = [
+    "_6ns _8ru _59hp",
+    "_6ns _5jmm _5jmm",
+    "_5jmm _5pat _5srp",
+    "_5jmm _5pat _5uch",
+    "_5uch _5jmm _5pat"
+];
