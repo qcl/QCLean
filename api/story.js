01cb0efb36cf3f85cca3375b6419364ce11495c2
Qing-Cheng Li <qc.linux@gmail.com>
2014-03-13 10:37:31 +0800

    update api

--- api/story.js
+++ api/story.js
@@ -5,5 +5,6 @@ qclean.storyClassNames = [
     "_6ns _5jmm _5jmm",
     "_5jmm _5pat _5srp",
     "_5jmm _5pat _5uch",
-    "_5uch _5jmm _5pat"
+    "_5uch _5jmm _5pat",
+    "_4-u2 mbm _5jmm _5pat _5v3q _5sq8 _5x16"
 ];
