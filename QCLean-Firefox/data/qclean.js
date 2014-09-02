764b064b8556717fb1622a5f1fa17f8fb55fe95f
Qing-Cheng Li <qc.linux@gmail.com>
2014-09-02 17:23:51 +0800

    Fix QCLean for Firefox didn't work bugs. And add new features.
    Add button on Firefox ui, let user know it is woring or not.

--- QCLean-Firefox/data/qclean.js
+++ QCLean-Firefox/data/qclean.js
@@ -1,9 +1,11 @@
 var qclean = qclean || {};
 
+console.log("Load qclean.js");
+
 qclean.removeADsLink = function(){
 
     var adsLink = document.getElementsByClassName("adsCategoryTitleLink");
-
+    var combo = 0;
     while(adsLink.length>0){
         for(var i=0;i<adsLink.length;i++){
             var target = adsLink[i].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
@@ -11,8 +13,24 @@ qclean.removeADsLink = function(){
             console.log('Remove ads');
         }
         adsLinkk = document.getElementsByClassName("adsCategoryTitleLink");
+        combo++;
+        if(combo>3){
+            //TODO - need notify
+            console.log("Found but can not remove Q____Q");
+        }
     }
 };
+
+if(qclean.storyClassNames==undefined){
+    qclean.storyClassNames = [
+        "_6ns _8ru _59hp",
+        "_6ns _5jmm _5jmm",
+        "_5jmm _5pat _5srp",
+        "_5jmm _5pat _5uch",
+        "_5uch _5jmm _5pat",
+        "_4-u2 mbm _5jmm _5pat _5v3q _5sq8 _5x16"
+        ];
+}
 
 qclean.removeSponsored = function(){
 
@@ -25,19 +43,31 @@ qclean.removeSponsored = function(){
             while(n.parentNode.nodeName!="BODY"){
                 if(n.parentNode.nodeName=="LI"){
                     if(n.parentNode.hasAttribute("class")&&n.parentNode.getAttribute("class").match("uiStreamStory")){
+                        //for fb old ui user
                         found = true;
                         break;
                     }
                 }else if(n.parentNode.nodeName=="DIV"){
                     if(n.parentNode.hasAttribute("class")){
-                        if(n.parentNode.getAttribute("class").match("_6ns _8ru _59hp")){
-                            //for new fb newsfeed
-                            found = true;
+                        var className = n.parentNode.getAttribute("class");
+                        for(var i = 0; i<qclean.storyClassNames.length; i++){
+                            if(className.match(qclean.storyClassNames[i])){
+                                found = true;
+                                break;
+                            }
+                        }
+                        if(found){
                             break;
-                        }else if(n.parentNode.getAttribute("class").match("_5jmm _5pat _5srp")){                    
-                            //fb change its class name, jizz
-                            found = true;
-                            break;
+                        }else{
+                            if(n.parentNode.hasAttribute("data-ft")){
+                                var dataFt = JSON.parse(n.parentNode.getAttribute("data-ft"));
+                                if(dataFt["mf_story_key"]!=undefined){
+                                    found = true;
+                                    console.log("class "+className+" may be story class name.");
+                                    break;
+                                }
+
+                            }    
                         }
                     }
                 }
@@ -53,10 +83,11 @@ qclean.removeSponsored = function(){
         combo++;
         if(combo>3){
             //TODO - notify there is some thing new/unknow
+            console.log("Found but can not remove Q____Q");
             break;
         }
     }
-}
+};
 
 qclean.removeSponsored();
 qclean.removeADsLink();
@@ -81,13 +112,14 @@ if(XMLHttpRequest.prototype.overrideByQCLean===undefined){
         }
     }
     XMLHttpRequest.prototype.overrideByQCLean = true;
+
 }
 
 //Override DIV appendChild
 if(HTMLDivElement.prototype.overrideByQCLean===undefined){
 
     var originDivAppend = HTMLDivElement.prototype.appendChild;
-    HTMLDivElement.prototype.appendChild = function(){ 
+    HTMLDivElement.prototype.appendChild = function(){
         originDivAppend.apply(this,arguments); 
         qclean.removeADsLink();
     
--- QCLean-Firefox/lib/main.js
+++ QCLean-Firefox/lib/main.js
@@ -1,11 +1,52 @@
-var pageMod = require("sdk/page-mod");
-var data = require("sdk/self").data;
+var qclean = {} || qclean;
 
+var pageMod = require("sdk/page-mod");
+var tabs = require("sdk/tabs");
+var data = require("sdk/self").data;
+var { ActionButton } = require("sdk/ui/button/action");
+
+/* Add content script for facebook's page. */
 pageMod.PageMod({
     include: "*.facebook.com",
-    contentScriptFile: [data.url("qclean.js")],
+    contentScript:
+        'var qclean = qclean || {};' +
+        'qclean.script = document.createElement("script");' +
+        'qclean.script.src = "'+data.url("qclean.js")+'";'+
+        '(document.head||document.documentElement).appendChild(qclean.script);',
     contentStyleFile: data.url("remove.css"),
     contentScriptWhen: "ready"
 });
 
+/* Add button on ui */
+qclean.uiButton = ActionButton({
+    id: "qclean-button",
+    label: "QCLean",
+    icon: {
+        "16":"./qclean16.png",
+        "32":"./qclean32.png"
+    },
+    onClick: function(state){
+        //TODO
+        //qclean.uiBtnClick(state);
+    },
+    disabled: true
+});
+
+//TODO
+qclean.uiBtnClick = function(state){
+    console.log(state);
+};
+
+/* Enable button only in *.facebook.com */
+qclean.fbUrlRegExp = new RegExp("^(http://|https://).*\.facebook\.com/");
+
+qclean.uiBtnEnable = function(tab,enable){
+    qclean.uiButton.state(tab,{disabled: !enable});
+};
+
+tabs.on("ready",function(tab){
+    console.log("Something ready");
+    console.log(tab.url)
+    qclean.uiBtnEnable(tab,qclean.fbUrlRegExp.test(tab.url));
+});
 
