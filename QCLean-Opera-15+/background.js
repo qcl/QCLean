220f9a286f4550e49649c2aa09084f2c82d3f963
Qing-Cheng Li <qc.linux@gmail.com>
2014-09-11 19:25:27 +0800

    Update Opera 15+ to 0.4.0.1
    Update icons
    Remove ga code
    Fix bugs

--- QCLean-Opera-15+/background.js
+++ QCLean-Opera-15+/background.js
@@ -1,4 +1,4 @@
-var qclean_version = "0.3.1.1";
+var qclean_version = "0.4.0.1";
 
 var qclean_fb_url_regexp = new RegExp("^(http://|https://).*\.facebook\.com/");
 
@@ -9,18 +9,4 @@ function qclean_check_fb_url(tabId, changeInfo, tab) {
 };
 
 chrome.tabs.onUpdated.addListener(qclean_check_fb_url);
-
-/* GA */
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-3607701-4']);
-_gaq.push(['_trackPageview']);
-
-_gaq.push(['_trackEvent','OperaExtVersion',qclean_version]);
-
-(function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = 'https://ssl.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
 
--- QCLean-Opera-15+/qclean.js
+++ QCLean-Opera-15+/qclean.js
@@ -1,9 +1,10 @@
+console.log("Load qclean.js");
 var qclean = qclean || {};
 
 qclean.removeADsLink = function(){
 
     var adsLink = document.getElementsByClassName("adsCategoryTitleLink");
-
+    var combo = 0;
     while(adsLink.length>0){
         for(var i=0;i<adsLink.length;i++){
             var target = adsLink[i].parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
@@ -11,8 +12,25 @@ qclean.removeADsLink = function(){
             console.log('Remove ads');
         }
         adsLinkk = document.getElementsByClassName("adsCategoryTitleLink");
+        combo++;
+        if(combo>3){
+            //TODO - report more useful information for fix bugs.
+            console.log("Found but can not remove Q____Q");
+            break;
+        }
     }
 };
+
+if(qclean.storyClassNames==undefined){
+    qclean.storyClassNames = [
+        "_6ns _8ru _59hp",
+        "_6ns _5jmm _5jmm",
+        "_5jmm _5pat _5srp",
+        "_5jmm _5pat _5uch",
+        "_5uch _5jmm _5pat",
+        "_4-u2 mbm _5jmm _5pat _5v3q _5sq8 _5x16"
+        ];
+}
 
 qclean.removeSponsored = function(){
 
@@ -31,14 +49,25 @@ qclean.removeSponsored = function(){
                     }
                 }else if(n.parentNode.nodeName=="DIV"){
                     if(n.parentNode.hasAttribute("class")){
-                        if(n.parentNode.getAttribute("class").match("_6ns _8ru _59hp")){
-                            //for new fb newsfeed
-                            found = true;
+                        var className = n.parentNode.getAttribute("class");
+                        for(var i = 0; i<qclean.storyClassNames.length; i++){
+                            if(className.match(qclean.storyClassNames[i])){
+                                found = true;
+                                break;
+                            }
+                        }
+                        if(found){
                             break;
-                        }else if(n.parentNode.getAttribute("class").match("_5jmm _5pat _5srp")){                    
-                            //fb change its class name, jizz
-                            found = true;
-                            break;
+                        }else{
+                            if(n.parentNode.hasAttribute("data-ft")){
+                                var dataFt = JSON.parse(n.parentNode.getAttribute("data-ft"));
+                                if(dataFt["mf_story_key"]!=undefined){
+                                    found = true;
+                                    console.log("class "+className+" may be story class name.");
+                                    break;
+                                }
+
+                            }
                         }
                     }
                 }
@@ -54,10 +83,11 @@ qclean.removeSponsored = function(){
         combo++;
         if(combo>3){
             //TODO - notify there is some thing new/unknow
+            console.log("Found but can not remove Q____Q");
             break;
         }
     }
-}
+};
 
 qclean.removeSponsored();
 qclean.removeADsLink();
