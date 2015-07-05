26ae165e59c6c59b60f6d8ed9c10bdd58c85e91a
Qing-Cheng Li <qc.linux@gmail.com>
2015-07-06 00:00:21 +0800

    Support hiding recommend games in chat bar and collapse sections

--- QCLean-Chrome-Experiment/qclean.js
+++ QCLean-Chrome-Experiment/qclean.js
@@ -11,20 +11,46 @@ qclean.version = manifest.version;
 console.log("Load qclean.js ("+qclean.version+")");
 
 /* QCLean judge functions */
-qclean.judgeFunction = qclean.judgeFunction || {};
+qclean.hiding = qclean.hiding || {};
 
-qclean.judgeFunction.sponsoredStoryOnNewsFeed = function(element) {
+qclean.hiding.isSponsoredStoryOnNewsFeed = function(element) {
     if(element.dataset.ft && JSON.parse(element.dataset.ft).mf_story_key){
         return true;
     }
     return false;
 };
 
-qclean.judgeFunction.sponsoredADs = function(element){
+qclean.hiding.isSponsoredADs = function(element){
     if(element.className=="ego_section"){
         return true;
     }
     return false;
+};
+
+qclean.hiding.isGameInChatBar = function (element) {
+    return (element.id == "pagelet_canvas_nav_content"); 
+};
+
+/* QCLean collaspe header and container functions */
+qclean.collaspe = qclean.collaspe || {};
+
+qclean.collaspe.contentComponentFinder = function (element) {
+    var header = element.querySelector(".uiHeaderTitle");
+    var container = element.querySelector(".ego_unit_container");
+
+    if (header && container) {
+        return {
+            "header"    : header,
+            "container" : container,
+            "title"     : header
+        };
+    }
+
+    return undefined;
+};
+
+qclean.collaspe.contentHandler = function (event) {
+    // no-op
 };
 
 /* QCLean */
@@ -33,16 +59,46 @@ qclean.feature = qclean.feature || {};
 
 // Feature: hide sponsored story on news feed
 qclean.feature.hideSponsoredStoryOnNewsFeed = {
-    "judgeFunction" : qclean.judgeFunction.sponsoredStoryOnNewsFeed,
+    "type"          : "hide",
+    "judgeFunction" : qclean.hiding.isSponsoredStoryOnNewsFeed,
     "name"          : "hideSponsoredStoryOnNewsFeed",
     "description"   : "Hide sponsored story on news feed"
 };
 
 // Feature: hide sponsored ADs
 qclean.feature.hideSponsoredADs = {
-    "judgeFunction" : qclean.judgeFunction.sponsoredADs,
+    "type"          : "hide",
+    "judgeFunction" : qclean.hiding.isSponsoredADs,
     "name"          : "hideSponsoredADs",
     "description"   : "Hide sponsored AD on photo view and persional view"
+};
+
+// Feature: hide recommended game in chat bar
+qclean.feature.hideRecommendedGameInChatBar = {
+    "type"          : "hide",
+    "judgeFunction" : qclean.hiding.isGameInChatBar,
+//  "afterHidingHandler" : qclean.hiding.adjustChatBodyHeight,
+    "name"          : "hideRecommendedGameInChatBar",
+    "description"   : "Hide recommended game in chat bar"
+};
+
+// Feature: collaspe contnet
+qclean.feature.collaspeSidebarContent = {
+    "type"          : "collaspe",
+    "componentFinder" : qclean.collaspe.contentComponentFinder,
+    "collaspeHandler" : qclean.collaspe.contentHandler,
+    "name"          : "collaspeSidebarContent",
+    "description"   : "Collaspe sidebar content"
+};
+
+// Feature: collaspe recommended games
+// TODO
+qclean.feature.collaspeRecommendedGame = {
+    "type"          : "collaspe",
+    "componentFinder" : undefined,
+    "collaspeHandler" : undefined,
+    "name"          : "collaspeRecommendedGame",
+    "description"   : "Collaspe recommended game"
 };
 
 /* QCLean hide element framework */
@@ -57,6 +113,9 @@ qclean.framework._hideElementByTargetChild = function(target, featureDesc){
                 element.style.display = "none";
                 target.dataset.qclean = "done";
                 console.log("Hide something ("+featureDesc.name+")");
+                //if (featureDesc.afterHidingHandler) {
+                //    featureDesc.afterHidingHandler();
+                //}
                 break;
             }
             element = element.parentElement;
@@ -72,12 +131,54 @@ qclean.framework._hideElementByTargetChild = function(target, featureDesc){
 
 qclean.framework.hideElementsByTargetChildSelector = function(selectors, featureDesc){
     var targetChilds = document.querySelectorAll(selectors);
-    //if(targetChilds.length>0){
-    //    console.log(targetChilds.length);
-    //}
     for(var i=0; i<targetChilds.length; i++){
         if(!targetChilds[i].dataset.qclean){
             qclean.framework._hideElementByTargetChild(targetChilds[i], featureDesc);
+        }
+    }
+};
+
+qclean.framework._setupCollaspeComponent = function(component, handler) {
+    var header = component.header;
+    var container = component.container;
+    var title = component.title;
+
+    if (!header.dataset.qcleanCollaspe) {
+        console.log("add new collaspe area");
+        header.classList.add("qcleanClickable");
+        container.classList.add("qcleanHide");
+        if (title) {
+            title.innerHTML = title.innerHTML + " ...";
+        }
+        header.dataset.qcleanCollaspe = "true";
+        header.qcleanCollaspeContainer = container;
+        header.onclick = function (event) {
+            handler(event);
+            if (this.dataset.qcleanCollaspe == "true") {
+                this.qcleanCollaspeContainer.classList.remove("qcleanHide");
+                this.dataset.qcleanCollaspe = "false";
+            } else {
+                this.qcleanCollaspeContainer.classList.add("qcleanHide");
+                this.dataset.qcleanCollaspe = "true";
+            }
+            //TODO: i13n
+        }
+    }
+};
+
+qclean.framework._collaspeElement = function(element, featureDesc) {
+    var component = featureDesc.componentFinder(element);
+    if (component) {
+        qclean.framework._setupCollaspeComponent(component, featureDesc.collaspeHandler);
+        element.dataset.qclean = "done";
+    }
+};
+
+qclean.framework.collaspeElementsBySelector = function(selector, featureDesc) {
+    var targetAreas = document.querySelectorAll(selector);
+    for (var i = 0; i < targetAreas.length; i++) {
+        if (!targetAreas[i].dataset.qclean) {
+            qclean.framework._collaspeElement(targetAreas[i], featureDesc); 
         }
     }
 };
@@ -92,6 +193,12 @@ var qcleanObserver = new window.MutationObserver(function(mutation, observer){
 
     // hide sponsered ADs
     qclean.framework.hideElementsByTargetChildSelector(".adsCategoryTitleLink:not([data-qclean])", qclean.feature.hideSponsoredADs);
+
+    // collaspe sidebar content
+    qclean.framework.collaspeElementsBySelector(".ego_section:not([data-qclean]):not([style])", qclean.feature.collaspeSidebarContent); 
+
+    // hide recommended game in chat bar
+    qclean.framework.hideElementsByTargetChildSelector("#pagelet_canvas_nav_content:not([data-qclean])", qclean.feature.hideRecommendedGameInChatBar);
 });
 
 qcleanObserver.observe(document, {
