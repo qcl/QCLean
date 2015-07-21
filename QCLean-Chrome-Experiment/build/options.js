8ce4667e4e2a0fea1401b4f7be238e2c22fe67b8
Qing-Cheng Li <qc.linux@gmail.com>
2015-07-21 23:27:45 +0800

    #14 Build Option component

--- QCLean-Chrome-Experiment/build/options.js
+++ QCLean-Chrome-Experiment/build/options.js
@@ -1,4 +1,18 @@
+var Option = React.createClass({displayName: "Option",
+    render: function () {
+        return (
+            React.createElement("div", null, 
+                React.createElement("label", {className: "mdl-switch mdl-js-switch mdl-js-ripple-effect", for: "switch-id"}, 
+                    React.createElement("input", {type: "checkbox", id: "switch-id", className: "mdl-switch__input", checked: true}), 
+                    React.createElement("span", {className: "mdl-switch__label"}, "OptionName")
+                ), 
+                React.createElement("p", {className: "option-desc"}, "Description")
+            )
+        );
+    }
+});
+
 React.render(
-    React.createElement("h3", null, "Settings"),
+    React.createElement(Option, null),
     document.getElementById('setting')
 );
--- QCLean-Chrome-Experiment/src/options.jsx
+++ QCLean-Chrome-Experiment/src/options.jsx
@@ -1,4 +1,18 @@
+var Option = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <label className="mdl-switch mdl-js-switch mdl-js-ripple-effect" for="switch-id">
+                    <input type="checkbox" id="switch-id" className="mdl-switch__input" checked />
+                    <span className="mdl-switch__label">OptionName</span>
+                </label>
+                <p className="option-desc">Description</p>
+            </div>
+        );
+    }
+});
+
 React.render(
-    <h3>Settings</h3>,
+    <Option />,
     document.getElementById('setting')
 );
