15f0455a771444f31fba29878d0321926e6c5730
Qing-Cheng Li <qc.linux@gmail.com>
2015-07-23 23:35:45 +0800

    #14 Try event handle

--- QCLean-Chrome-Experiment/build/options.js
+++ QCLean-Chrome-Experiment/build/options.js
@@ -1,9 +1,13 @@
 var Option = React.createClass({displayName: "Option",
+    switchDidChanged: function(event) {
+        console.log("switchDidChanged");
+        console.log(event.target);
+    },
     render: function () {
         return (
             React.createElement("div", null, 
                 React.createElement("label", {className: "mdl-switch mdl-js-switch mdl-js-ripple-effect", htmlFor: "switch-id"}, 
-                    React.createElement("input", {type: "checkbox", id: "switch-id", className: "mdl-switch__input"}), 
+                    React.createElement("input", {type: "checkbox", id: "switch-id", className: "mdl-switch__input", onChange: this.switchDidChanged}), 
                     React.createElement("span", {className: "mdl-switch__label"}, "OptionName")
                 ), 
                 React.createElement("p", {className: "option-desc"}, "Description")
--- QCLean-Chrome-Experiment/src/options.jsx
+++ QCLean-Chrome-Experiment/src/options.jsx
@@ -1,9 +1,13 @@
 var Option = React.createClass({
+    switchDidChanged: function(event) {
+        console.log("switchDidChanged");
+        console.log(event.target);
+    },
     render: function () {
         return (
             <div>
-                <label className="mdl-switch mdl-js-switch mdl-js-ripple-effect" htmlFor="switch-id">
-                    <input type="checkbox" id="switch-id" className="mdl-switch__input"/>
+                <label className="mdl-switch mdl-js-switch mdl-js-ripple-effect" htmlFor="switch-id" >
+                    <input type="checkbox" id="switch-id" className="mdl-switch__input" onChange={this.switchDidChanged} />
                     <span className="mdl-switch__label">OptionName</span>
                 </label>
                 <p className="option-desc">Description</p>
