537b78836571c49a59c28fbd8a4edd2ccfa9245c
Qing-Cheng Li <qc.linux@gmail.com>
2015-08-16 15:07:29 +0800

    #14 use state and props

--- QCLean-Chrome-Experiment/build/options.js
+++ QCLean-Chrome-Experiment/build/options.js
@@ -1,13 +1,31 @@
 var Option = React.createClass({displayName: "Option",
+    getInitialState: function() {
+        // TODO get init state from chrome's setting
+        console.log("getInitialState");
+
+        // just check here already can read props
+        console.log(this.props);
+
+        return {
+            'isSeetingApplied': false
+        };
+    },
     switchDidChanged: function(event) {
         console.log("switchDidChanged");
-        console.log(event.target);
+        //console.log(event.target);
+        // UI changed but the state of input not change, need to set state.
+        // UI may change by other js ?
+        this.setState({"isSeetingApplied": !this.state.isSeetingApplied});
     },
     render: function () {
         return (
             React.createElement("div", null, 
-                React.createElement("label", {className: "mdl-switch mdl-js-switch mdl-js-ripple-effect", htmlFor: "switch-id"}, 
-                    React.createElement("input", {type: "checkbox", id: "switch-id", className: "mdl-switch__input", onChange: this.switchDidChanged, checked: false}), 
+                React.createElement("label", {className: "mdl-switch mdl-js-switch mdl-js-ripple-effect", htmlFor: this.props.id}, 
+                    React.createElement("input", {type: "checkbox", 
+                            id: this.props.id, 
+                            className: "mdl-switch__input", 
+                            onChange: this.switchDidChanged, 
+                            checked: this.state.isSeetingApplied}), 
                     React.createElement("span", {className: "mdl-switch__label"}, "OptionName")
                 ), 
                 React.createElement("p", {className: "option-desc"}, "Description")
@@ -17,6 +35,6 @@ var Option = React.createClass({displayName: "Option",
 });
 
 React.render(
-    React.createElement(Option, null),
+    React.createElement(Option, {id: "switch-id"}),
     document.getElementById('setting')
 );
--- QCLean-Chrome-Experiment/src/options.jsx
+++ QCLean-Chrome-Experiment/src/options.jsx
@@ -1,13 +1,31 @@
 var Option = React.createClass({
+    getInitialState: function() {
+        // TODO get init state from chrome's setting
+        console.log("getInitialState");
+
+        // just check here already can read props
+        console.log(this.props);
+
+        return {
+            'isSeetingApplied': false
+        };
+    },
     switchDidChanged: function(event) {
         console.log("switchDidChanged");
-        console.log(event.target);
+        //console.log(event.target);
+        // UI changed but the state of input not change, need to set state.
+        // UI may change by other js ?
+        this.setState({"isSeetingApplied": !this.state.isSeetingApplied});
     },
     render: function () {
         return (
             <div>
-                <label className="mdl-switch mdl-js-switch mdl-js-ripple-effect" htmlFor="switch-id" >
-                    <input type="checkbox" id="switch-id" className="mdl-switch__input" onChange={this.switchDidChanged} checked={false} />
+                <label className="mdl-switch mdl-js-switch mdl-js-ripple-effect" htmlFor={this.props.id} >
+                    <input  type="checkbox" 
+                            id={this.props.id} 
+                            className="mdl-switch__input" 
+                            onChange={this.switchDidChanged} 
+                            checked={this.state.isSeetingApplied} />
                     <span className="mdl-switch__label">OptionName</span>
                 </label>
                 <p className="option-desc">Description</p>
@@ -17,6 +35,6 @@ var Option = React.createClass({
 });
 
 React.render(
-    <Option />,
+    <Option id="switch-id" />,
     document.getElementById('setting')
 );
