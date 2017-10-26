bf1c6dffc6cac2f43b32bc9fe1b134c1a216fa0c
Qing-Cheng Li <qc.linux@gmail.com>
2017-10-26 08:20:16 +0800

    revmoe react and rewrite script of options page

--- Web-Extension/options.js
+++ Web-Extension/options.js
@@ -1,77 +1,4 @@
-//var browser = browser || undefined;
-//var chrome = browser || chrome;
-
 var browser = browser || chrome;
-let parser = new DOMParser();
-
-var Option = React.createClass({displayName: "Option",
-    componentWillMount: function() {
-        console.log("componentWillMount");
-        // get setting title and description
-        this.title = chrome.i18n.getMessage(this.props.titleText);
-        this.desc = chrome.i18n.getMessage(this.props.desc);
-    },
-    componentDidMount: function() {
-        console.log("componentDidMount");
-        
-        // get applied state
-        var settingKey = this.props.id;
-        var defaultValue = this.props.defaultValue;
-
-        var settingQuery = {};
-        settingQuery[settingKey] = defaultValue;
-        chrome.storage.sync.get(settingQuery, this.handleSetting);
-    },
-    getInitialState: function() {
-        console.log("getInitialState");
-        
-        var defaultValue = this.props.defaultValue;
-        
-        // return init state
-        return {
-            'isSeetingApplied': defaultValue
-        };
-    },
-    handleSetting: function(items) {
-        var settingKey = this.props.id;
-        var defaultValue = this.props.defaultValue;
-        var isSeetingApplied = items[settingKey];
-        if (isSeetingApplied == undefined) {
-            isSeetingApplied = defaultValue;
-        }
-        this.setState({"isSeetingApplied": isSeetingApplied});
-    },
-    switchDidChanged: function(event) {
-        console.log("switchDidChanged");
-        var settingKey = this.props.id;
-        var items = {};
-        items[settingKey] = !this.state.isSeetingApplied;
-        chrome.storage.sync.set(items,function(){
-            // no-op for now.
-        });
-        this.setState({"isSeetingApplied": !this.state.isSeetingApplied});
-    },
-    render: function () {
-        console.log("render called");
-        var labelClasses = "mdl-switch mdl-js-switch mdl-js-ripple-effect is-upgraded";
-        if (this.state.isSeetingApplied) {
-            labelClasses += " is-checked";
-        }
-        return (
-            React.createElement("div", null, 
-                React.createElement("label", {className: labelClasses, htmlFor: this.props.id}, 
-                    React.createElement("input", {type: "checkbox", 
-                            id: this.props.id, 
-                            className: "mdl-switch__input", 
-                            onChange: this.switchDidChanged, 
-                            checked: this.state.isSeetingApplied}), 
-                    React.createElement("span", {className: "mdl-switch__label"}, this.title)
-                ), 
-                React.createElement("p", {className: "option-desc"}, this.desc)
-            )
-        );
-    }
-});
 
 // TODO mv setting to another file, let it can be shared
 var qcleanSettings = [{
@@ -108,11 +35,9 @@ var qcleanSettings = [{
 
 let optionTemplate = document.querySelector('template#option');
 let targetElement = document.querySelector('div#qclean-settings');
-console.log(optionTemplate);
 
 let renderOptions = (targetElement, template, settings) => {
     for(let setting of settings) {
-        console.log(setting);
         // clone element from template
         let dom = document.importNode(template.content, true);
 
@@ -131,24 +56,27 @@ let renderOptions = (targetElement, template, settings) => {
         query[key] = setting.default;
 
         browser.storage.sync.get(query, (items) => {
-            console.log(key);
-            console.log(items[key]);
+            console.log(`${key} is ${items[key]}`);
             input.defaultChecked = items[key];
             if (input.defaultChecked) {
                 label.classList.add('is-checked');
             } else {
                 label.classList.remove('is-checked');
             }
-            console.log(input);
         });
 
         title.textContent = titleText;
         desc.textContent = descText;
         label.setAttribute('for',setting.key);
         input.id = key;
-
-        console.log(label);
-        console.log(dom);
+        input.onchange = (e) => {
+            let applied = input.checked;
+            let query = {};
+            query[key] = input.checked;
+            browser.storage.sync.set(query, () => {
+                console.log(`${key} changed to ${query[key]}`);
+            });
+        };
 
         targetElement.appendChild(dom);
     }
@@ -156,35 +84,24 @@ let renderOptions = (targetElement, template, settings) => {
 
 renderOptions(targetElement, optionTemplate, qcleanSettings);
 
-React.render(
-    React.createElement("div", null, 
-        qcleanSettings.map(function(setting){
-            return React.createElement(Option, {id: setting.key, 
-                            defaultValue: setting.default, 
-                            titleText: setting.title, 
-                            desc: setting.desc}); 
-        })
-    ),
-    document.getElementById('setting')
-);
-
 // init and i18n
-var changeContentWithI18Message = function(cssQueryString, message){
-    var messageText = chrome.i18n.getMessage(message);
-    if(messageText!=undefined && messageText.length > 0){
-        console.log("message"+message+" is "+messageText);
-        var dom = document.querySelector(cssQueryString);
-        dom.innerHTML = messageText;
+let changeContentWithI18Message = (cssQueryString, message) => {
+    let messageText = browser.i18n.getMessage(message);
+    if (messageText != undefined && messageText.length > 0){
+        console.log(`message ${message} is ${messageText}`);
+        let dom = document.querySelector(cssQueryString);
+        dom.textContent = messageText;
     }
 };
 
-var initOptionsPage = function() {
-    var settingTitle = chrome.i18n.getMessage("extSettings");
-    document.title = settingTitle;
-    var manifest = chrome.runtime.getManifest();
-    document.querySelector("a#version").innerHTML = manifest.version;
+let initOptionsPage = function() {
+    let settingTitle = browser.i18n.getMessage("extSettings");
+    let manifest = browser.runtime.getManifest();
 
-    var messages = [{
+    document.title = settingTitle;
+    document.querySelector("a#version").textContent = manifest.version;
+
+    let messages = [{
         message: "extSettings",
         query: "h3#settingTitle"
     }, {
@@ -199,9 +116,12 @@ var initOptionsPage = function() {
     }, {
         message: "extReportBug",
         query: "a#report"
+    }, {
+        message: "extPrivacyPolicy",
+        query: "a#privacypolicy"
     }];
-    for(var i in messages){
-        var message = messages[i];
+
+    for(let message of messages){
         changeContentWithI18Message(message.query, message.message);
     }
 };
