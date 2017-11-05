de3ac6ceb85608806806763b61a73cbe2b71f89f
Qing-Cheng Li <qc.linux@gmail.com>
2017-11-05 20:06:24 +0800

    add privacy policy and bump version to 0.5.8.6

--- QCLean-Chrome/popup.js
+++ QCLean-Chrome/popup.js
@@ -1,3 +1,5 @@
+var browser = chrome;
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -8,23 +10,30 @@ ga('create', 'UA-3607701-10', 'auto');
 ga('set','checkProtocolTask',null);
 ga('send','pageview');
 
-var manifest = chrome.runtime.getManifest();
+var manifest = browser.runtime.getManifest();
 ga('send','event','openPopupPage',manifest.version);
 
 // QCLean popup script
-var load = function(){
+let load = () => {
     // setup links
-    var settingBtn = document.querySelector("a#settingLink");
-    settingBtn.innerHTML = chrome.i18n.getMessage("extSettings");
-    settingBtn.onclick = function(e) {
+    let settingBtn = document.querySelector("a#settingLink");
+    settingBtn.textContent = browser.i18n.getMessage("extSettings");
+    settingBtn.onclick = (e) => {
+        e.preventDefault();
+        browser.tabs.create({
+            url: browser.runtime.getURL("options.html")
+        });
         ga('send','event','openSettingFromPopup',manifest.version);
     };
 
-    var reportBtn = document.querySelector("a#bugReportLink");
-    reportBtn.innerHTML = chrome.i18n.getMessage("extReportBug");
-    reportBtn.onclick = function(e) {
+    let reportBtn = document.querySelector("a#bugReportLink");
+    reportBtn.textContent = browser.i18n.getMessage("extReportBug");
+    reportBtn.onclick = (e) => {
         ga('send','event','reportBugFromPopup',manifest.version);
     };
+
+    let ppBtn = document.querySelector("a#privacyPolicyLink");
+    ppBtn.textContent = browser.i18n.getMessage('extPrivacyPolicy');
 };
 
 window.onload = load;
